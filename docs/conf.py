eb5e2b085e95310e5de28e43ce5a170f19ec1a29
mhadam <michael@hadam.us>
2020-04-24 10:59:47 -0400

    Update docs version

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2020, Michael Hadam"
 author = "Michael Hadam"
 
 # The full version, including alpha/beta/rc tags
-release = "3.0.0"
+release = "3.1.0"
 
 # -- General configuration ---------------------------------------------------
 
