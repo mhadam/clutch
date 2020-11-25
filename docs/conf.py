72d2abd70c3bbda0ab536da7075e73a2dbca9747
mhadam <michael@hadam.us>
2020-11-24 21:56:00 -0500

    Change accessor fields from optional, modify IdsArg

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2020, Michael Hadam"
 author = "Michael Hadam"
 
 # The full version, including alpha/beta/rc tags
-release = "4.2.2"
+release = "5.0.0"
 
 # -- General configuration ---------------------------------------------------
 
