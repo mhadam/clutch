6cb35f0c4b6c7b5fcb6a7d68f3375fbb57271262
mhadam <michael@hadam.us>
2020-11-24 21:56:00 -0500

    Change accessor fields from optional, modify IdsArg

--- clutch/schema/user/method/shared.py
+++ clutch/schema/user/method/shared.py
@@ -4,4 +4,4 @@ from clutch.compat import Literal
 
 TorrentId = Union[int, str]
 
-IdsArg = Union[int, Set[TorrentId], Literal["recently_active"]]
+IdsArg = Union[int, Set[TorrentId], Set[int], Set[str], Literal["recently_active"]]
