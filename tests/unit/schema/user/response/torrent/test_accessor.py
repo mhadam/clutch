bce3352244739b999fa6b8e29aa7a33a8a921190
mhadam <michael@hadam.us>
2020-11-25 20:02:38 -0500

    Fix schema changes, use intermediate models

---
+++ tests/unit/schema/user/response/torrent/test_accessor.py
@@ -0,0 +1,12 @@
+from clutch.schema.user.response.torrent.accessor import TorrentAccessorObject
+
+
+def test_optional_fields():
+    data = {
+        "error": "some string"
+    }
+
+    result = TorrentAccessorObject.construct(**data)
+
+    # as of writing now, construct creates a model without any of the missing fields
+    assert result.error == "some string"
